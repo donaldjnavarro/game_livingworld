d8f3ff155ac71bf916292cccf683339d49eaad13
donaldjnavarro <donaldjnavarro@gmail.com>
2020-06-05 13:14:11 -0400

    saving

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import cmd
+import json
+from datetime import datetime
 
-class matter(location):
+class matter(object):
+    """Physical things are composed of matter"""
+    def __init__(self, name, location="here"):
+        self.name = name
+        self.location = location
 
+    def __getitem__(self, item):
+        """Enable bracket syntax for calling attributes of objects created with this class"""
+        return getattr(self, item)
 
 class prompt(cmd.Cmd):
     """Global prompt. This class creates the default prompt interface. All menus should inherit this class"""
@@ -24,9 +33,36 @@ class prompt(cmd.Cmd):
     def do_create(self, arg):
         if arg:
             print("You attempt to create",arg)
-            created = arg
+            newMatter = matter(arg)
+            reality[arg] = newMatter.__dict__
+
+    def do_look(self, arg):
+        if not arg:
+            for thing in reality:
+                if reality[thing]["location"] == "here":
+                    print(reality[thing]["name"],"is here.")
+
+    def do_destroy(self, arg):
+        """Destroy anything in reality by name"""
+        if arg in reality:
+            reality.pop(arg)
+
+    def do_save(self, arg):
+        """Save everything in reality to a JSON file"""
+        if arg:
+            filename = arg
+        else:
+            filename = "untitled-"+datetime.now().strftime("%Y-%m-%d-%H%M%S")
+        filename = filename+".json" # extension
+        filename = "logs/"+filename # folder
+        # Save as JSON file
+        json_output = open(filename, "w") 
+        json.dump(reality, json_output, indent = 6) 
+        json_output.close()
+        print("Reality saved to",filename)
 
 if __name__ == '__main__':
     running = True
+    reality = {} # An object to collect everything that is created
     while running == True:
         prompt().cmdloop()
--- readme.md
+++ readme.md
@@ -17,5 +17,6 @@
 - Do weapons need to be distinct, or should all objects have things like weight and hardness and sharpness defined and then an attack action can use any object, or even your own body
 - Do we need multiple cross sections of class definitions? Type (mammals, reptiles) vs Aspects (hands, speech)
 
-
-
+# TODO
+- Need unique identifiers so multiple objects can be created with the same name
+  
